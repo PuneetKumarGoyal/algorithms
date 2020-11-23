d067780e2caa387fe7a93a84cca0355007661c90
goyalpuneet1 <goyal.puneet1@tcs.com>
2020-11-23 19:32:08 +0100

    adding more files

--- src/binarytree/BinarySearchTree.java
+++ src/binarytree/BinarySearchTree.java
@@ -7,6 +7,12 @@ import java.util.Map;
 /*
 In BST, left node is smaller than root, and right node is larger than root
  */
+
+/**
+ *          5
+ *        3   6
+ *       1 4 3 8
+ */
 public class BinarySearchTree {
 
     static class Node {
@@ -36,7 +42,24 @@ public class BinarySearchTree {
                 .max(Comparator.naturalOrder()).get();
     }
 
+    public boolean isBST(Node root) {
+        if(root == null) return false;
+        //all left nodes are smaller than right nodes
+        return isBSTUtil(root, Integer.MAX_VALUE, Integer.MIN_VALUE) ;
+    }
 
+    private boolean isBSTUtil(Node node, int rightmin, int leftmax ) {
+        if(node == null) return true;
+        if(node.left != null && node.left.data < rightmin) return true;
+        if(node.right != null && node.right.data > leftmax) return true;
+        return isBSTUtil(node.left, rightmin, (leftmax<node.left.data)?node.left.data:leftmax)
+                && isBSTUtil(node.right, (rightmin>node.right.data)?node.right.data:rightmin, leftmax)
+        ;
+    }
+
+    public void print(Node root) {
+
+    }
 
     public static void main(String[] args) {
 
